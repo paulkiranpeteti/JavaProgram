6b6fce1f9c490279d164dae61c7eff1e6ee98788
paulkiranpeteti <110374869+paulkiranpeteti@users.noreply.github.com>
2024-03-13 08:29:31 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,7 +1,9 @@
-public class Jenkins
-{
-  public static void main(String args[])
-  {
-      System.out.println("Welcome to Jenkins!");
-  }
+public class Jenkins {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=x+y;
+
+      System.out.println("Sum of x+y = " + z);
+    }
 }
